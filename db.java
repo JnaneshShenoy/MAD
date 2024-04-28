f70221ecb140df8f4c7b60a2321207e5b5db2801
Jnanesh Shenoy <148083819+JnaneshShenoy@users.noreply.github.com>
2024-04-28 23:34:04 +0530

    code fix

--- db.java
+++ db.java
@@ -1,64 +1,78 @@
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-   private EditText usnEditText, nameEditText, phoneEditText, searchUsnEditText;
-   private Button insertButton, searchButton;
-   private SQLiteDatabase database;
-
-
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_main);
-
-
-       usnEditText = findViewById(R.id.usn_edit_text);
-       nameEditText = findViewById(R.id.name_edit_text);
-       phoneEditText = findViewById(R.id.phone_edit_text);
-       searchUsnEditText = findViewById(R.id.search_usn_edit_text);
-       insertButton = findViewById(R.id.insert_button);
-       searchButton = findViewById(R.id.search_button);
-
-
-       insertButton.setOnClickListener(this);
-       searchButton.setOnClickListener(this);
-   }
-
-
-   @SuppressLint("Range")
-   @Override
-   public void onClick(View view) {
-       String usn, name, phone;
-
-
-       usn = usnEditText.getText().toString();
-       name = nameEditText.getText().toString();
-       phone = phoneEditText.getText().toString();
-
-
-       try {
-           database = this.openOrCreateDatabase("stud01", MODE_PRIVATE, null);
-           database.execSQL("CREATE TABLE IF NOT EXISTS test(usn VARCHAR(20), name VARCHAR(20), phone VARCHAR(20));");
-
-
-           if (view.getId() == R.id.insert_button) {
-               database.execSQL("INSERT INTO test VALUES('" + usn + "','" + name + "','" + phone + "');");
-               Toast.makeText(getApplicationContext(), "Row inserted successfully", Toast.LENGTH_SHORT).show();
-           } else if (view.getId() == R.id.search_button) {
-               Cursor cursor = database.rawQuery("SELECT * FROM test WHERE usn='" + searchUsnEditText.getText().toString() + "';", null);
-               if (cursor.getCount() != 1) {
-                   Toast.makeText(getApplicationContext(), "Invalid USN", Toast.LENGTH_SHORT).show();
-               } else {
-                   cursor.moveToNext();
-                   Intent intent = new Intent(Intent.ACTION_DIAL);
-                   intent.setData(Uri.parse("tel:" + cursor.getString(cursor.getColumnIndex("phone"))));
-                   startActivity(intent);
-               }
-           }
-       } catch (SQLiteException e) {
-           Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
-       } finally {
-           if (database != null) {
-               database.close();
-           }
-       }
-   }
-}
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button b1,b2;
+    EditText e1,e2,e3;
+    SQLiteDatabase db=null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+ 
+ 
+        e1=findViewById( R.id.editText1 );
+        e2=findViewById( R.id.editText2 );
+        e3=findViewById( R.id.editText3 );
+        b1=findViewById( R.id.button1);
+        b2=findViewById( R.id.button2);
+ 
+ 
+        b1.setOnClickListener( this );
+        b2.setOnClickListener( this );
+    }
+ 
+ 
+    @SuppressLint("Range")
+    @Override
+    public void onClick(View view) {
+        String sname,sage,saddress;
+        sname=e1.getText().toString();
+        sage=e2.getText().toString();
+        saddress=e3.getText().toString();
+        try {
+            db = this.openOrCreateDatabase( "stud", MODE_PRIVATE, null );
+            db.execSQL( "create table if not exists test(name varchar(20),age varchar(20),address varchar(50));" );
+ 
+ 
+ 
+ 
+            if (view.getId() == b1.getId()) {
+                db.execSQL( "insert into test values('"+sname+"','"+sage+"','"+saddress+"');" );
+                Toast.makeText( getApplicationContext(),"Row inserted successfully",Toast.LENGTH_SHORT ).show();
+ 
+ 
+ 
+ 
+            }
+            if(view.getId()==b2.getId())
+            {
+                Cursor c=db.rawQuery( "select * from test",null );
+                String all="";
+ 
+ 
+                if(c!=null)
+                {
+                    if(c.moveToFirst())
+                    {
+                        do{
+                            sname=c.getString( c.getColumnIndex("name" ));
+                            sage=c.getString(c.getColumnIndex( "age"));
+                            saddress=c.getString( c.getColumnIndex("address") );
+                            all=all+sname+"\t\t"+sage+"\t\t"+saddress+"\n";
+ 
+ 
+                        }while(c.moveToNext());
+                        Toast.makeText( this,all,Toast.LENGTH_SHORT ).show();
+                    }
+                }
+            }
+        }
+        catch (SQLiteException sq)
+        {
+            Toast.makeText( getApplicationContext(),"Could not create or open database",Toast.LENGTH_SHORT ).show();
+        }
+        finally {
+            if(db!=null)
+                db.close();
+        }
+    }
+ }
+ 
